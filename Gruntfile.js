e3a6906be173fd3b40e96bc4e35c89d60a6879b7
Mahmoud Ragab <refat333@gmail.com>
2014-10-31 22:15:59 -0700

    replace tabs with spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,107 +1,107 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON("package.json"),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
 
-		meta: {
-			banner: "/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today(\"yyyy-mm-dd\") %> */\n"
-		},
+        meta: {
+            banner: "/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today(\"yyyy-mm-dd\") %> */\n"
+        },
 
-		// configure tasks
-		clean: {
-			dev: {
-				src: ['dev']
-			},
-			dist: {
-				src: ['dist']
-			}
-		},
+        // configure tasks
+        clean: {
+            dev: {
+                src: ['dev']
+            },
+            dist: {
+            src: ['dist']
+            }
+        },
 
-		concat: {
-			js: {
-				src: ["src/js/**/*.js"],
-				dest: "dev/app.js"
-			},
-			css: {
-				src: "src/css/**/*.css",
-				dest: "dev/app.css"
-			}
-		},
-		
-		cssmin: {
-			compress: {
-				files: {
-					"dist/app.min.css": "<%= concat.css.dest %>"
-				}
-			}
-		},
+        concat: {
+            js: {
+                src: ["src/js/**/*.js"],
+                dest: "dev/app.js"
+            },
+            css: {
+                src: "src/css/**/*.css",
+                dest: "dev/app.css"
+            }
+        },
+        
+        cssmin: {
+            compress: {
+                files: {
+                    "dist/app.min.css": "<%= concat.css.dest %>"
+                }
+            }
+        },
 
-		uglify: {
-			js: {
-				src: "<%= concat.js.dest %>",
-				dest: "dist/app.min.js"
-			}
-		},
+        uglify: {
+            js: {
+                src: "<%= concat.js.dest %>",
+                dest: "dist/app.min.js"
+            }
+        },
 
-		watch: {
-			options: {
-				livereload: true
-			},
-			js: {
-				files: ["<%= concat.js.src %>"],
-				tasks: "concat:js"
-			},
-			css: {
-				files: ["<%= concat.css.src %>"],
-				tasks: "concat:css"
-			},
-			homepage: {
-				files: ["<%= homepage.template %>"],
-				tasks: ["homepage:dev"]
-			}
-		},
+        watch: {
+            options: {
+                livereload: true
+            },
+            js: {
+                files: ["<%= concat.js.src %>"],
+                tasks: "concat:js"
+            },
+            css: {
+                files: ["<%= concat.css.src %>"],
+                tasks: "concat:css"
+            },
+            homepage: {
+                files: ["<%= homepage.template %>"],
+                tasks: ["homepage:dev"]
+            }
+        },
 
-		homepage: {
-			template: "src/index.us",
-			dev: {
-				dest: "dev/index.html",
-				context: {
-					js: "app.js",
-					css: "app.css"
-				}
-			},
-			dist: {
-				dest: "dist/index.html",
-				context: {
-					js: "app.min.js",
-					css: "app.min.css"
-				}
-			}
-		},
+        homepage: {
+            template: "src/index.us",
+            dev: {
+                dest: "dev/index.html",
+                context: {
+                    js: "app.js",
+                    css: "app.css"
+                }
+            },
+            dist: {
+                dest: "dist/index.html",
+                context: {
+                    js: "app.min.js",
+                    css: "app.min.css"
+                }
+            }
+        },
 
-		express: {
-			all: {
-				options: {
-					port: 9000,
-					hostname: "localhost",
-					bases: ["dev"],
-					livereload: true
-				}
-			}
-		}
-	});
+        express: {
+            all: {
+                options: {
+                    port: 9000,
+                    hostname: "localhost",
+                    bases: ["dev"],
+                    livereload: true
+                }
+            }
+        }
+    });
 
-	// load tasks from npm
-	grunt.loadNpmTasks("grunt-contrib-concat");
-	grunt.loadNpmTasks("grunt-contrib-watch");
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-cssmin");
-	grunt.loadNpmTasks("grunt-contrib-clean");
-	grunt.loadNpmTasks("grunt-express");
+    // load tasks from npm
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-express");
 
-	grunt.loadTasks("tasks");
+    grunt.loadTasks("tasks");
 
-	// setup the workflow
-	grunt.registerTask("dev", ["clean", "concat", "homepage:dev", "express", "watch"]);
-	grunt.registerTask("dist", ["clean", "concat", "uglify", "cssmin", "homepage:dist"]);
-	grunt.registerTask("default", "dev");
+    // setup the workflow
+    grunt.registerTask("dev", ["clean", "concat", "homepage:dev", "express", "watch"]);
+    grunt.registerTask("dist", ["clean", "concat", "uglify", "cssmin", "homepage:dist"]);
+    grunt.registerTask("default", "dev");
 }
